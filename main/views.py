c6b36617d128cac186e732a5f7424073ded589ce
itssraeesa <asmalraeesa0@gmail.com>
2024-12-28 17:20:15 +0500

    Added docstrings to views.py

--- main/views.py
+++ main/views.py
@@ -1,16 +1,40 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-from django.shortcuts import render
 from .models import Song
 
 def home(request):
+    """
+    Renders the home page of the website.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered home page.
+    """
     return render(request, 'main/home.html')
 
 def about(request):
+    """
+    Renders the about page of the website.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered about page.
+    """
     return render(request, 'main/about.html')
 
 def songs(request):
+    """
+    Retrieves all the songs from the database and renders the songs page.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered songs page with a list of songs.
+    """
     songs = Song.objects.all()
     return render(request, 'main/songs.html', {'songs': songs})
+
